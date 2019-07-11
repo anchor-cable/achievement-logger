942c9199219e7cd21f94dcd27b8c5f7bf8dc1df6
Tomoya Suzuki <forever.blue.moon@gmail.com>
2019-07-11 21:29:49 +0900

    first commit

---
+++ server/achievement-logger/app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Users;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+
+class AuthController extends Controller
+{
+    private function jwt(User $user)
+    {
+        $signer = new Sha256();
+        $token = (new Builder())->setIssuer('http://achievement-logger')
+            ->setAudience('http://achievement-logger')
+            ->setId(uniqid(), true)
+            ->setIssuedAt(time())
+            ->setNotBefore(time() + 60)
+            ->setExpiration(time() + 3600)
+            ->set('uid', $user->id)
+            ->sign($signer, env('JWT_SECRET'))
+            ->getToken();
+
+        return $token;
+    }
+
+    public function authenticate(Request $request)
+    {
+        $this->validate($request, [
+            'email'    => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = Users::where('email', $request->input('email'))->first();
+
+        if (!$user) {
+            return response()->json(['error' => 'ログインできませんでした。'], 400);
+        }
+
+        return response()->json(['error' => 'ここで！'], 400);
+
+        if (Hash::check($request->input('password'), $user->password)) {
+            return response()->json($this->jwt($user)->__toString(), 200);
+        }
+
+        return response()->json(['error' => 'ログインできませんでした。'], 400);
+    }
+}
